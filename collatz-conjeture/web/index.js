469ec95cad5a8298d247db54071e9df9c82dd861
CyRstudent <a1433hsaez@iesmarmediterraneo.org>
2022-02-12 12:07:36 +0100

    fix: Catch error when n is NaN

--- collatz-conjeture/web/index.js
+++ collatz-conjeture/web/index.js
@@ -1,17 +1,32 @@
 const nArr = [];
 let n = Number(prompt('Introduce un numero positivo: '));
-nArr.push(n);
-do {
-	if (n % 2 == 1) {
-		n *= 3;
-		n++;
-	} else if (n % 2 == 0) {
-		n = n / 2;
-	}
+
+if (n == NaN) {
+	document.write(`
+	<body>
+		<p style="text-align:center">
+			<h3>
+				Tienes que introducir un numero antes de dar al botón de acceptar!
+			</h3>
+			<h5>
+				No intentes romperme la proxima vez porque no va a funcionar
+			</h5>
+		</p>
+	</body>
+	`);
+} else {
 	nArr.push(n);
-} while (n !== 1);
-console.log('Done');
-document.writeln(`
+	do {
+		if (n % 2 == 1) {
+			n *= 3;
+			n++;
+		} else if (n % 2 == 0) {
+			n = n / 2;
+		}
+		nArr.push(n);
+	} while (n !== 1);
+	console.log('Done');
+	document.writeln(`
 <html>
 	<body>
 		<p style="text-align:center">
@@ -20,3 +35,4 @@ document.writeln(`
 	</body>
 </html>
 `);
+}
