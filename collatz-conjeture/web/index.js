121b2e1a45a60d39947f3ac2eaeeb753ff82e6a7
CyRstudent <a1433hsaez@iesmarmediterraneo.org>
2022-02-12 12:30:14 +0100

    fix: I thought i fixed it on last commit but i didn't  Now it actually catches the error  It also centers correctly the text!

--- collatz-conjeture/web/index.js
+++ collatz-conjeture/web/index.js
@@ -1,38 +1,67 @@
-const nArr = [];
-let n = Number(prompt('Introduce un numero positivo: '));
+/*
+		<p style="text-align:center">
+		Simulador de la conjetura de collatz</p>
+ */
 
-if (n == NaN) {
-	document.write(`
+const throwErr = async () =>{
+	const text = `
 	<body>
-		<p style="text-align:center">
-			<h3>
+		<h3>
+			<p style="text-align:center">
 				Tienes que introducir un numero antes de dar al botón de acceptar!
-			</h3>
-			<h5>
+			</p>
+		</h3>
+		<h5>
+			<p style="text-align:center">
 				No intentes romperme la proxima vez porque no va a funcionar
-			</h5>
-		</p>
+			</p>
+		</h5>
 	</body>
-	`);
+	`;
+	const res = await document.write(text);
+	return res;
+};
+
+
+const nArr = [];
+
+const ans = prompt('Introduce un numero positivo: ', '1');
+
+if (
+	typeof ans === 'undefined' ||
+	ans === null
+) {
+	throwErr();
 } else {
-	nArr.push(n);
-	do {
-		if (n % 2 == 1) {
-			n *= 3;
-			n++;
-		} else if (n % 2 == 0) {
-			n = n / 2;
-		}
+	let n = Number(ans);
+	if (
+		isNaN(n) ||
+		n == 0 ||
+		n == null
+
+	) {
+		throwErr();
+	} else {
 		nArr.push(n);
-	} while (n !== 1);
-	console.log('Done');
-	document.writeln(`
-<html>
-	<body>
-		<p style="text-align:center">
-			<h4>${nArr.join(' => ')}</h4>
-		</p>
-	</body>
-</html>
-`);
+		do {
+			if (n % 2 == 1) {
+				n *= 3;
+				n++;
+			} else if (n % 2 == 0) {
+				n = n / 2;
+			}
+			nArr.push(n);
+		} while (n !== 1);
+		console.log('Done');
+		document.writeln(`
+	<html>
+		<body>
+			<p style="text-align:center">
+				<h4>${nArr.join(' => ')}</h4>
+			</p>
+		</body>
+	</html>
+	`);
+	}
 }
+
